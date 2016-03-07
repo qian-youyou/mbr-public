65c3a873a5b97b17e80924b7a2a4f57ac4a97f2c
Nicolas Emiliani <nicolas@motrixi.com>
2016-03-07 17:00:23 -0300

    MRP-2274 handle multiple relay requests.

--- src/relay/relay.cpp
+++ src/relay/relay.cpp
@@ -2,6 +2,7 @@
 
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
+#include "rapidjson/document.h"
 
 #include <glog/logging.h>
 
@@ -13,6 +14,8 @@
 #include <event2/keyvalq_struct.h>
 
 #include <boost/algorithm/string.hpp>
+
+#include <algorithm>
 
 MTX::Relay::Relay(const rapidjson::Document& conf, struct event_base *base){
     LOG(INFO) << "building configuration ...";
@@ -48,6 +51,14 @@ MTX::Relay::relay_cb(struct evhttp_request *req, void *arg){
     relay_placeholder* p = (relay_placeholder*)arg;
     p->self->process_relay(req, p->original_req, p->connection);
     delete p;
+}
+
+void
+MTX::Relay::multiple_relay_cb(struct evhttp_request *req, void *arg){
+    multiple_relay_placeholder* p = (multiple_relay_placeholder*)arg;
+    bool cleanup = p->self->process_multiple_relay(req, p);
+    if(cleanup)
+        delete p;
 }
 
 std::string
@@ -110,9 +121,29 @@ MTX::Relay::process_request(struct evhttp_request *req){
     for(auto it = qs_map.begin(); it != qs_map.end(); ++it)
         LOG(INFO) << "\t" << it->first << " : " << it->second;
 
-    std::pair<std::string, unsigned short> banker_uri =
-        get_relay_uri(path, cmdtype, qs_map);
+    std::string parent_account =
+        get_parent_account(path, cmdtype, qs_map);
 
+    if(parent_account.size() && parent_account != "*"){
+        // single shoot
+        single_shoot(req, parent_account, uri);
+    }else if(parent_account.size()){
+        // it's a multiple request
+        multiple_shoot(req, uri);
+    }else{
+        evhttp_send_reply(req, 500, "Error", NULL);
+    }
+
+}
+
+void
+MTX::Relay::single_shoot(
+        struct evhttp_request *req,
+        const std::string& parent_account,
+        const std::string& uri){
+
+    std::pair<std::string, unsigned short> banker_uri;
+    banker_uri = get_relay_uri(parent_account);
     if(!banker_uri.first.size()){
         evhttp_send_reply(req, 500, "Error", NULL);
         return;
@@ -157,6 +188,56 @@ MTX::Relay::process_request(struct evhttp_request *req){
         evhttp_request_get_command(req), uri.c_str());
 }
 
+
+void
+MTX::Relay::multiple_shoot(
+    struct evhttp_request *req, const std::string& uri){
+
+    // get the body
+    struct evbuffer *buf = evhttp_request_get_input_buffer(req);
+    std::string body = get_body(buf);
+
+    multiple_relay_placeholder* holder = new multiple_relay_placeholder;
+    holder->self = this;
+    holder->original_req = req;
+    holder->response_counter = 0;
+
+    shard_map::iterator it;
+    for(it = shards.begin(); it != shards.end(); ++it){
+
+        std::pair<std::string, unsigned short> banker_uri = it->second;
+        // create the connection
+        struct evhttp_connection* conn =
+            evhttp_connection_base_new(base, NULL,
+                banker_uri.first.c_str(), banker_uri.second);
+        holder->connections.push_back(conn);
+
+        // create the relay request
+        struct evhttp_request *relay_req =
+            evhttp_request_new(multiple_relay_cb, holder);
+
+        // set the headers
+        struct evkeyval *header;
+        struct evkeyvalq *headers = evhttp_request_get_input_headers(req);
+        for (header = headers->tqh_first; header;
+            header = header->next.tqe_next){
+            evhttp_add_header(
+                relay_req->output_headers, header->key, header->value);
+        }
+
+        //set the body
+        struct evbuffer * relay_buf =
+            evhttp_request_get_output_buffer(relay_req);
+        evbuffer_add_printf(relay_buf, "%s", body.c_str());
+
+        // shoot
+        LOG(INFO) << "shooting shard " << it->first << " at " <<
+            it->second.first << ":" << it->second.second;
+        evhttp_make_request(conn, relay_req,
+            evhttp_request_get_command(req), uri.c_str());
+    }
+}
+
 void
 MTX::Relay::process_relay(
         evhttp_request *relay_req,
@@ -180,12 +261,50 @@ MTX::Relay::process_relay(
     evhttp_connection_free(relay_conn);
 }
 
-std::pair<std::string, unsigned short>
-MTX::Relay::get_relay_uri(
-            const std::string& path,
-            const std::string& cmdtype,
-            std::map<std::string, std::string>& qs_map){
+bool
+MTX::Relay::process_multiple_relay(
+                    evhttp_request *relay_req,
+                    multiple_relay_placeholder* holder){
 
+    //copy the relayed request body into the original body
+    struct evbuffer* buf =
+        evhttp_request_get_input_buffer(relay_req);
+    std::string body = get_body(buf);
+    holder->bodies.push_back(body);
+
+    holder->response_counter += 1;
+
+    if(holder->response_counter !=
+            (int)(holder->connections.size())){
+        return false;
+    }
+
+    // we got all the answers, we can reply now
+    std::string result_body = add_replies(holder->bodies);
+    LOG(INFO) << "result_body : " << result_body;
+    struct evbuffer* req_buf =
+        evhttp_request_get_output_buffer(holder->original_req);
+    evbuffer_add_printf(req_buf, "%s", result_body.c_str());
+
+    // send the reply
+    evhttp_send_reply(holder->original_req,
+        evhttp_request_get_response_code(relay_req),
+        "OK",
+        req_buf);
+
+    // clean up connections
+    for(std::size_t i = 0; i < holder->connections.size(); ++i){
+        evhttp_connection_free(holder->connections[i]);
+    }
+
+    return true;
+}
+
+std::string
+MTX::Relay::get_parent_account(
+                   const std::string& path,
+                   const std::string& cmdtype,
+                   std::map<std::string, std::string>& qs_map){
     std::vector<std::string> path_parts;
     boost::split(path_parts, path, boost::is_any_of("/"));
 
@@ -201,7 +320,9 @@ MTX::Relay::get_relay_uri(
     if(action == "accounts" && cmdtype == "POST"){
         // POST /v1/accounts
         parent = get_parent_account(qs_map["accountName"]);
-    }else if(action == "accounts" && cmdtype == "GET"){
+    }else if(action == "accounts" &&
+                 cmdtype == "GET" &&
+                 path_parts.size() == 4){
         // GET /v1/accounts/<accountName>
         parent = get_parent_account(path_parts[3]);
     }else if((
@@ -232,14 +353,16 @@ MTX::Relay::get_relay_uri(
             path == "/v1/sumary")
                 && (cmdtype == "GET")){
         parent = "*";
-        // TODO : in this cases we need to shoot every shard,
         // collect the data and the return it
     }
 
-    if(!parent.size()){
+    if(!parent.size())
         LOG(ERROR) << "unable to find account name";
-        return std::make_pair<std::string, unsigned short>("",0);
-    }
+    return parent;
+}
+
+std::pair<std::string, unsigned short>
+MTX::Relay::get_relay_uri(const std::string& parent){
 
     LOG(INFO) << "parent account : " << parent;
     unsigned int hash = SDBMHash(parent);
@@ -289,3 +412,29 @@ MTX::Relay::get_body(struct evbuffer *buf){
     }
     return body;
 }
+
+std::string
+MTX::Relay::add_replies(const std::vector<std::string>& bodies){
+    if(!bodies.size())
+        return "";
+
+    rapidjson::Document result;
+    result.Parse(bodies[0].c_str());
+    rapidjson::Document::AllocatorType& allocator = result.GetAllocator();
+    if(result.IsArray()){
+        for(std::size_t i = 1; i < bodies.size(); ++i){
+            rapidjson::Document tmp;
+            tmp.Parse(bodies[i].c_str());
+            for(auto it = tmp.Begin(); it != tmp.End(); ++it){
+                result.PushBack(*it, allocator);
+            }
+        }
+    }
+
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    result.Accept(writer);
+
+    return buffer.GetString();
+}
+
--- src/relay/relay.h
+++ src/relay/relay.h
@@ -24,11 +24,28 @@ struct Relay{
 
 private :
 
+    struct relay_placeholder{
+        Relay* self;
+        evhttp_request* original_req;
+        evhttp_connection* connection;
+    };
+
+    struct multiple_relay_placeholder{
+        Relay* self;
+        evhttp_request* original_req;
+        std::vector<evhttp_connection*> connections;
+        std::vector<std::string> bodies;
+        int response_counter;
+    };
+
     void process_request(struct evhttp_request *req);
 
     void process_relay(evhttp_request *relay_req,
                        evhttp_request *original_req,
                        evhttp_connection *relay_conn);
+
+    bool process_multiple_relay(evhttp_request *relay_req,
+                                multiple_relay_placeholder* holder);
 
     std::string
     get_command(struct evhttp_request *req);
@@ -45,27 +62,41 @@ private :
     std::pair<std::string, unsigned short>
     get_banker_uri(unsigned int hash);
 
+    std::string
+    get_parent_account(const std::string& path,
+                       const std::string& cmdtype,
+                       std::map<std::string, std::string>& qs_map);
+
     std::pair<std::string, unsigned short>
-    get_relay_uri(const std::string& path,
-                  const std::string& cmdtype,
-                  std::map<std::string, std::string>& qs_map);
+    get_relay_uri(const std::string& parent);
 
     // callback for the http relay response
     static void
     relay_cb(struct evhttp_request *req, void *arg);
 
+    // callback for the http relay response
+    static void
+    multiple_relay_cb(struct evhttp_request *req, void *arg);
+
+    void
+    single_shoot(
+        struct evhttp_request *req,
+        const std::string& parent_account,
+        const std::string& uri);
+
+    void
+    multiple_shoot(
+        struct evhttp_request *req,
+        const std::string& uri);
+
     std::string get_body(struct evbuffer *buf);
+
+    std::string add_replies(const std::vector<std::string>& bodies);
 
     typedef std::map<int, std::pair<std::string, unsigned short>> shard_map;
     shard_map shards;
 
     struct event_base* base;
-
-    struct relay_placeholder{
-        Relay* self;
-        evhttp_request* original_req;
-        evhttp_connection* connection;
-    };
 };
 
 }
